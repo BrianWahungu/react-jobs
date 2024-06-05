1b5b392c09f97cfd89852098f45c9ff8214ddfc3
BrianWahungu <cepteinhawks@gmail.com>
2024-06-05 14:51:38 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,58 @@ import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage'
 import JobPage, {jobLoader} from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-  <Route path='/' element={<MainLayout/>}>
-  <Route index element={<HomePage />} />
-  <Route path='/jobs' element={<JobsPage />} />
-  <Route path='/add-job' element={<AddJobPage />} />
-  <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader}/>
-  <Route path='*' element={<NotFoundPage />} />
-
-  </Route>
-)
-);
 const App = () => {
+  // Add New Job
+
+  const addJob = async (newJob) => {
+    const res = await fetch('https://api.npoint.io/eece4f58692932709f82/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob)
+    });
+    return;
+  };
+  
+  // Delete Job
+
+  const deleteJob = async (id) => {
+    const res = await fetch(`https://api.npoint.io/eece4f58692932709f82/api/jobs/${id}`, {
+      method: 'DELETE',
+    });
+    return;
+  };
+
+  // Update Job
+
+  const updateJob = async (job) => {
+    const res = await fetch(`https://api.npoint.io/eece4f58692932709f82/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(job)
+    });
+    return;
+  }
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+    <Route path='/' element={<MainLayout/>}>
+    <Route index element={<HomePage />} />
+    <Route path='/jobs' element={<JobsPage />} />
+    <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>} />
+    <Route path='/edit-job/:id' element={<EditJobPage updateJobSubmit={updateJob}/>} loader={jobLoader}/>
+    <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob}/>} loader={jobLoader}/>
+    <Route path='*' element={<NotFoundPage />} />
+  
+    </Route>
+  )
+  );
+
   return <RouterProvider router={router}/>;
 };
 
